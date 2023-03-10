8baf0f4fa8f4916c9495e9c3364b5b3e3396166c
Adriano Amorim <adryanoamorim@hotmail.com>
2023-03-10 18:19:05 -0300

    feat:Adiciona header e inicio do scroll tela home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,65 @@
 
+import * as H from "../../components/Header/styled"
+import * as S from "./styled"
 
-export const Home =()=>{
-    return(
+
+
+export const Home = () => {
+    return (
         <>
-        Home
+            <H.ToolbarRoot asChild>
+                <header>
+                    <H.TitleHeader>
+                        MEUS CONTATOS
+                    </H.TitleHeader>
+                    <H.BoxButtons>
+                        <H.ToolbarButton>
+                            <H.VoltarIcon />
+                        </H.ToolbarButton>
+                        <H.ToolbarButton>
+                            <H.DeletarIcon />
+                        </H.ToolbarButton>
+                        <H.ToolbarButton>
+                            <H.SaveIcon />
+                        </H.ToolbarButton>
+                        <H.ToolbarButton>
+                            <H.ConfigIcon />
+                        </H.ToolbarButton>
+                    </H.BoxButtons>
+                </header>
+            </H.ToolbarRoot>
+
+            <S.Main>
+                <S.BoxListContacts>
+
+                    <S.ViewportListContacts>
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                            <span>OI</span>
+                        </div>
+                    </S.ViewportListContacts>
+
+                    <S.ScrollbarListContacts orientation="horizontal">
+                        <S.ThumbListContacts />
+                    </S.ScrollbarListContacts>
+
+                    <S.ScrollbarListContacts orientation="vertical">
+                        <S.ThumbListContacts />
+                    </S.ScrollbarListContacts>
+
+                    <S.CornerListContacts />
+                </S.BoxListContacts>
+
+            </S.Main>
         </>
     )
 }
