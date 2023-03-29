6bb0b04f69c0bc2ea347539bc5a46f09e2d0428a
Adriano Amorim <adryanoamorim@hotmail.com>
2023-03-28 21:40:23 -0300

    feat:adicionado passagem de dados (fake) para tela de Visualizar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,12 +5,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { Main } from "../../components/MainComponent";
 import { HeaderContext } from "../../contexts/HeaderContext";
 import { getContacts } from "../../services/Api";
-
+import { ContactType } from "../../types/ContactType";
 import * as S from "./styled";
 
 export const Home = () => {
     const { setHeaderState } = useContext(HeaderContext);
-    const [dataContacts, setDataContacts] = useState(getContacts());
+    const [dataContacts, setDataContacts] = useState<ContactType[]>(getContacts());
     const navigate = useNavigate();
 
     //CONFIGURA OS BOTOES DO HEADER
@@ -19,12 +19,12 @@ export const Home = () => {
         btnSave: false,
         btnDelete: false,
         btnBack: false,
+        btnEditar:false,
         title: "Contatos"
     };
 
     //FAZ UM GET PARA PEGAR A LISTA COM TODOS OS CONTATOS CADASTRADOS
     const viewContact = (id:number) => {
-        console.log(id)
         navigate(`/editar/${id}`);
     }
 
@@ -42,10 +42,11 @@ export const Home = () => {
 
             <S.BoxCardContacts>
                 {
-                    dataContacts.contact.map((contato, i) =>
-                        <S.CardContacts key={i} onClick={()=> viewContact(contato.id)}>
+                    dataContacts.map((contato, i) =>
+                    
+                        <S.CardContacts key={i} onClick={()=> viewContact(contato.id?contato.id:0)}>
                             <PersonIcon className="iconContact" color="var(--bg-header)" width={20} height={20} />
-                            <S.NameContacts data-id={contato.id}>
+                            <S.NameContacts>
                                 {contato.nome}
                             </S.NameContacts>
                         </S.CardContacts>
